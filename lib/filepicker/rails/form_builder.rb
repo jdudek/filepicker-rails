4afeafd5d23f1811edd8a98577147ab0ad7bc321
Ewa Limanowka <ewa.limanowka@gmail.com>
2012-11-20 14:16:51 +0100

    Added more options to filepicker_field

--- lib/filepicker/rails/form_builder.rb
+++ lib/filepicker/rails/form_builder.rb
@@ -4,14 +4,27 @@ module Filepicker
 
       def filepicker_field(method, options = {})
         input_options = {
-          'value' => options[:value],
           'data-fp-apikey' => Filepicker::Rails.config.api_key,
+
           'data-fp-button-text' => options.fetch(:button_text, "Pick File"),
+
           'data-fp-button-class' => options[:button_class],
+
           'data-fp-mimetypes' => options[:mimetypes],
+
           'data-fp-option-container' => options[:container],
+
           'data-fp-option-multiple' => false,
-          'data-fp-option-services' => Array(options[:services]).join(",")
+
+          'data-fp-option-services' => Array(options[:services]).join(","),
+
+          'data-fp-drag-text' => options.fetch(:drag_text, "Or drop files here"),
+
+          'data-fp-drag-class' => options[:drag_class],
+
+          'onchange' => options[:onchange],
+
+          'value' => options[:value]
         }
 
         type = options[:dragdrop] ? 'filepicker-dragdrop' : 'filepicker'
