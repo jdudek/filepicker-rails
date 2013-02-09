6c35e071b0b4c1a42e23155a5a8aa565bb5febf7
Jan Dudek <jd@jandudek.com>
2013-02-09 17:07:56 +0100

    Added helper for async JavaScript

--- lib/filepicker/rails/view_helpers.rb
+++ lib/filepicker/rails/view_helpers.rb
@@ -8,6 +8,12 @@ module Filepicker
         javascript_include_tag "//api.filepicker.io/v0/filepicker.js"
       end
 
+      def filepicker_async_js_include_tag
+        javascript_tag <<-JS
+          (function(a){if(window.filepicker){return}var b=a.createElement("script");b.type="text/javascript";b.async=!0;b.src=("https:"===a.location.protocol?"https:":"http:")+"//api.filepicker.io/v1/filepicker.js";var c=a.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c);var d={};d._queue=[];var e="pick,pickMultiple,pickAndStore,read,write,writeUrl,export,convert,store,storeUrl,remove,stat,setKey,constructWidget,makeDropPane".split(",");var f=function(a,b){return function(){b.push([a,arguments])}};for(var g=0;g<e.length;g++){d[e[g]]=f(e[g],d._queue)}window.filepicker=d})(document);
+        JS
+      end
+
       def filepicker_save_button(text, url, mimetype, options = {})
         options[:data] ||= {}
         container = options.delete(:container)
